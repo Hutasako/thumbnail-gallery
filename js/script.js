cdf5119a68a6624a4fc32ab4d96a566be50d02e8
Hutasako <ejjungao@yahoo.com>
2021-04-15 11:49:44 -0400

    added preferred solution

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-const clickedImg = document.querySelectorAll(".thumb");
 const bigImg = document.querySelector(".bigimg");
+const theGallery = document.querySelector(`.gallery`)
 
-clickedImg.forEach(item =>{
-        item.addEventListener('click', ()=>{
-            console.log(`Source: ${item.src}, Alt: ${item.alt}`); 
-            bigImg.src =  item.src;
-            bigImg.alt = item.alt;
-    })
-})
+// const clickedImg = document.querySelectorAll(".thumb").forEach(item =>{
+//         item.addEventListener('click', ()=>{
+//             console.log(`Source: ${item.getAttribute('src')}, Alt: ${item.getAttribute('alt')}`); 
+//             bigImg.src =  item.getAttribute('src');
+//             bigImg.alt = item.getAttribute('alt');
+//     })
+// })
+
+const setThumb = function (event) {
+
+    // target refers to what was under the mouse when this event occurred
+   
+    const whichOne = event.target
+   
+   
+    if(whichOne.matches(`.thumb`)){
+   
+        let imgSrc = whichOne.getAttribute(`src`)
+   
+        let imgAlt = whichOne.getAttribute(`alt`)
+       
+       
+        // Update the big image's attributes
+       
+        bigImg.setAttribute(`src`, imgSrc)
+       
+        bigImg.setAttribute(`alt`, imgAlt)
+       
+    }
+   }
+     
+   // Listen to the entire gallery for a click
+   
+   theGallery.addEventListener(`click`, setThumb)
